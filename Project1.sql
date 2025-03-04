42d286f99d6fb123a6907391dfd26da21f4ee449
Aaron Campuzano <120442928+Brokebor@users.noreply.github.com>
2025-03-03 22:11:05 -0500

    Add files via upload

---
+++ Project1.sql
@@ -0,0 +1,9 @@
+use project1;
+describe cleaning;
+insert into cleaning
+values 
+(500, '09:00:00', '12:00:00', 200);
+describe clients;
+insert into clients
+values ('24', '123-456-7890', '123 Main St', 'clientA@example.com'),
+('23', '987-654-3210', '456 Elm St', 'clientB@example.com');
